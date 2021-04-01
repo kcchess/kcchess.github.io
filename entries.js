04182f5956032dab33d696aeb1c321f4826d1640
kcchess <81649189+kcchess@users.noreply.github.com>
2021-04-01 00:23:45 -0500

    Create entries.js

---
+++ entries.js
@@ -0,0 +1,32 @@
+/**
+ * Every entry must include a title and filename, the rest are optional.
+ * For simplicity in merges, please add to the bottom of the object.
+ *
+ * interface
+ * {
+ *     title: string // display name
+ *     filename: string // the name of your HTML file
+ *     description?: string // description that will be listed with your entry
+ *     author?: string // your name/tag that will be listed
+ *     github?: string // username on github that will display a link to your profile
+ *     compatibleBrowsers?: array // browsers that this page is compatible with
+ *     sponsored?: boolean // don't worry about adding this, this is done by the owner of the project, your submission will be rejected if set to true without permission.
+ * }
+ */
+
+const entries = [
+	{
+		title: "Puzzle - Mate in One",
+		filename: "mate_in_one.html",
+		description: "Chess tactics, with mate in one posiotions.",
+		author: "",
+		github: "kcchess",
+	},
+	{
+		title: "Puzzle - Mate in Three",
+		filename: "mate_in_three.html",
+		description: "Chess tactics, with mate in three posiotions.",
+		author: "",
+		github: "kcchess",
+	},
+];
